ac18a4e37be7652fad74364d3324051a65d488f0
Evgeny <evgen.990029@gmail.com>
2020-10-02 09:34:33 +0300

    Add project files.

---
+++ API/ApiResponse.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections.Generic;
+
+namespace RecDadataToExcel
+{
+    class ApiResponse 
+    {
+        
+        public List<Suggestion> Suggestions { get; set; }
+        public class Suggestion
+        {
+            public string value { get; set; }
+            public string unrestricted_value { get; set; }
+            public Data data { get; set; }
+        }
+
+        public class Data
+        {
+            public string kpp { get; set; }
+            public object capital { get; set; }
+            public Management management { get; set; }
+            public string branch_type { get; set; }
+            public int branch_count { get; set; }
+            public object source { get; set; }
+            public object qc { get; set; }
+            public string hid { get; set; }
+            public string type { get; set; }
+            public State state { get; set; }
+            public Opf opf { get; set; }
+            public Name name { get; set; }
+            public string inn { get; set; }
+            public string ogrn { get; set; }
+            public object okpo { get; set; }
+            public string okved { get; set; }
+            public object okveds { get; set; }
+            public object authorities { get; set; }
+            public object documents { get; set; }
+            public object licenses { get; set; }
+            public Address address { get; set; }
+            public object phones { get; set; }
+            public object emails { get; set; }
+            public object ogrn_date { get; set; }
+            public string okved_type { get; set; }
+        }
+
+        public class Address
+        {
+            public string value { get; set; }
+            public string unrestricted_value { get; set; }
+            public Data2 data { get; set; }
+        }
+        public class Management
+        {
+            public string name { get; set; }
+            public string post { get; set; }
+        }
+        public class State
+        {
+            public string status { get; set; }
+            //public long actuality_date { get; set; }
+            //public long registration_date { get; set; }
+            public object liquidation_date { get; set; }
+        }
+        public class Name
+        {
+            public string full_with_opf { get; set; }
+            public string short_with_opf { get; set; }
+            public object latin { get; set; }
+            public string full { get; set; }
+            public string @short { get; set; }
+        }
+        public class Opf
+        {
+            public object type { get; set; }
+            public string code { get; set; }
+            public string full { get; set; }
+            public string @short { get; set; }
+        }
+        public class Data2
+        {
+            public string postal_code { get; set; }
+            public string country { get; set; }
+            public string region_fias_id { get; set; }
+            public string region_kladr_id { get; set; }
+            public string region_with_type { get; set; }
+            public string region_type { get; set; }
+            public string region_type_full { get; set; }
+            public string region { get; set; }
+            public object area_fias_id { get; set; }
+            public object area_kladr_id { get; set; }
+            public object area_with_type { get; set; }
+            public object area_type { get; set; }
+            public object area_type_full { get; set; }
+            public object area { get; set; }
+            public string city_fias_id { get; set; }
+            public string city_kladr_id { get; set; }
+            public string city_with_type { get; set; }
+            public string city_type { get; set; }
+            public string city_type_full { get; set; }
+            public string city { get; set; }
+            public string city_area { get; set; }
+            public object city_district_fias_id { get; set; }
+            public object city_district_kladr_id { get; set; }
+            public string city_district_with_type { get; set; }
+            public string city_district_type { get; set; }
+            public string city_district_type_full { get; set; }
+            public string city_district { get; set; }
+            public object settlement_fias_id { get; set; }
+            public object settlement_kladr_id { get; set; }
+            public object settlement_with_type { get; set; }
+            public object settlement_type { get; set; }
+            public object settlement_type_full { get; set; }
+            public object settlement { get; set; }
+            public string street_fias_id { get; set; }
+            public string street_kladr_id { get; set; }
+            public string street_with_type { get; set; }
+            public string street_type { get; set; }
+            public string street_type_full { get; set; }
+            public string street { get; set; }
+            public string house_fias_id { get; set; }
+            public string house_kladr_id { get; set; }
+            public string house_type { get; set; }
+            public string house_type_full { get; set; }
+            public string house { get; set; }
+            public object block_type { get; set; }
+            public object block_type_full { get; set; }
+            public string block { get; set; }
+            public object flat_type { get; set; }
+            public object flat_type_full { get; set; }
+            public string flat { get; set; }
+            public object flat_area { get; set; }
+            public object square_meter_price { get; set; }
+            public object flat_price { get; set; }
+            public object postal_box { get; set; }
+            public string fias_id { get; set; }
+            public string fias_level { get; set; }
+            public string kladr_id { get; set; }
+            public string capital_marker { get; set; }
+            public string okato { get; set; }
+            public string oktmo { get; set; }
+            public string tax_office { get; set; }
+            public string tax_office_legal { get; set; }
+            public object timezone { get; set; }
+            public string geo_lat { get; set; }
+            public string geo_lon { get; set; }
+            public object beltway_hit { get; set; }
+            public object beltway_distance { get; set; }
+            public object metro { get; set; }
+            public string qc_geo { get; set; }
+            public object qc_complete { get; set; }
+            public object qc_house { get; set; }
+            public object history_values { get; set; }
+            public object unparsed_parts { get; set; }
+            public string source { get; set; }
+            public object qc { get; set; }
+        }
+        /* public class Management
+         {
+             public string name { get; set; }
+             public string post { get; set; }
+         }
+
+         public class State
+         {
+             public string status { get; set; }
+             public long actuality_date { get; set; }
+             public long registration_date { get; set; }
+             public object liquidation_date { get; set; }
+         }
+
+        
+
+         public class Name
+         {
+             public string full_with_opf { get; set; }
+             public string short_with_opf { get; set; }
+             public object latin { get; set; }
+             public string full { get; set; }
+             public string @short { get; set; }
+         }
+
+        
+
+         public class Address
+         {
+             public string value { get; set; }
+             public string unrestricted_value { get; set; }
+             public Data2 data { get; set; }
+         }
+
+         public class Data
+         {
+             public string kpp { get; set; }
+             public object capital { get; set; }
+             public Management management { get; set; }
+             public string branch_type { get; set; }
+             public int branch_count { get; set; }
+             public object source { get; set; }
+             public object qc { get; set; }
+             public string hid { get; set; }
+             public string type { get; set; }
+             public State state { get; set; }
+             public Opf opf { get; set; }
+             public Name name { get; set; }
+             public string inn { get; set; }
+             public string ogrn { get; set; }
+             public object okpo { get; set; }
+             public string okved { get; set; }
+             public object okveds { get; set; }
+             public object authorities { get; set; }
+             public object documents { get; set; }
+             public object licenses { get; set; }
+             public Address address { get; set; }
+             public object phones { get; set; }
+             public object emails { get; set; }
+             public long ogrn_date { get; set; }
+             public string okved_type { get; set; }
+         }
+
+         public class Suggestion
+         {
+             public string value { get; set; }
+             public string unrestricted_value { get; set; }
+             public Data data { get; set; }
+         }
+
+         public class RootObject
+         {
+             public List<Suggestion> suggestions { get; set; }
+         }
+
+     }*/
+    }
+    }
---
+++ API/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+
+namespace RecDadataToExcel
+{  
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+  
+            Excel.Application ex = new Microsoft.Office.Interop.Excel.Application();
+
+            ex.Workbooks.Open(@"*****.xls",
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                false, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+ 
+            Microsoft.Office.Interop.Excel.Worksheet ObjWorkSheet;
+            ObjWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ex.Sheets[1];
+
+            // Указываем номер столбца
+            int numCol = 2;
+
+            Excel.Range usedColumn = ObjWorkSheet.UsedRange.Columns[numCol];      
+            System.Array myvalues = (System.Array)usedColumn.Cells.Value2;
+           
+            var j = 0;
+            string[] strArray = myvalues.OfType<object>().Select(o => o.ToString()).ToArray();
+            Console.WriteLine(strArray.Length);
+            
+            foreach (var i in strArray)
+            {
+              
+                var str = strArray[j];
+
+                var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
+
+                //генерация запроса
+                HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+                req.Method = "POST";
+                req.Timeout = 10000;
+                req.Headers.Add("Authorization", "Token *******");
+                req.ContentType = "application/json";
+                req.Accept = "application/json";
+
+                //данные для отправки
+                var sentData = Encoding.UTF8.GetBytes("{ \"query\": \"" + strArray[j] + "\" }");
+                req.ContentLength = sentData.Length;
+                Stream sendStream = req.GetRequestStream();
+                sendStream.Write(sentData, 0, sentData.Length);
+
+                //получение ответа
+                var res = req.GetResponse() as HttpWebResponse;
+                var resStream = res.GetResponseStream();
+                var sr = new StreamReader(resStream, Encoding.UTF8);
+
+                var response = sr.ReadToEnd();
+
+                ApiResponse apiResponse = JsonConvert.DeserializeObject<ApiResponse>(response);
+                var x = 0;
+                                          
+                    foreach (var z in apiResponse.Suggestions)
+                    {
+
+                    // ОПФ
+                    if (apiResponse.Suggestions[x].data.opf != null)
+                    {
+                        ObjWorkSheet.Cells[1, "Q"] = "ОПФ";
+                        ObjWorkSheet.Cells[j + 1, "Q"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.opf.@short) ? "" : apiResponse.Suggestions[x].data.opf.@short;
+                    }
+
+                    //Полное Наименование
+                    if (apiResponse.Suggestions[x].data.name != null)
+                    {
+                        ObjWorkSheet.Cells[1, "S"] = "Полное Наименование";
+                        ObjWorkSheet.Cells[j + 1, "S"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.name.full_with_opf) ? "" : apiResponse.Suggestions[x].data.name.full_with_opf;
+                    }                   
+
+                    if (apiResponse.Suggestions[x].data != null)
+                    { 
+                        ObjWorkSheet.Cells[1, "R"] = "КПП";
+                        ObjWorkSheet.Cells[j + 1, "R"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.kpp) ? "" : apiResponse.Suggestions[x].data.kpp;                 
+                    }
+
+                    if (apiResponse.Suggestions[x].data.address.data != null)
+                    {
+                        ObjWorkSheet.Cells[1, "T"] = "Регион";
+                        ObjWorkSheet.Cells[j + 1, "T"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.region_with_type) ? "" : apiResponse.Suggestions[x].data.address.data.region_with_type;
+
+                        ObjWorkSheet.Cells[1, "U"] = "Район";
+                        ObjWorkSheet.Cells[j + 1, "U"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.city_district_with_type) ? "" : apiResponse.Suggestions[x].data.address.data.city_district_with_type;
+
+                        ObjWorkSheet.Cells[1, "V"] = "Город";
+                        ObjWorkSheet.Cells[j + 1, "V"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.city_with_type) ? "" : apiResponse.Suggestions[x].data.address.data.city_with_type;
+
+                        ObjWorkSheet.Cells[1, "W"] = "Улица";
+                        ObjWorkSheet.Cells[j + 1, "W"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.street_with_type) ? "" : apiResponse.Suggestions[x].data.address.data.street_with_type;
+
+                        ObjWorkSheet.Cells[1, "X"] = "Дом";
+                        ObjWorkSheet.Cells[j + 1, "X"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.house) ? "" : apiResponse.Suggestions[x].data.address.data.house;
+
+                        ObjWorkSheet.Cells[1, "Y"] = "Корпус";
+                        ObjWorkSheet.Cells[j + 1, "Y"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.block) ? "" : apiResponse.Suggestions[x].data.address.data.block;
+
+                        ObjWorkSheet.Cells[1, "Z"] = "Квартира";
+                        ObjWorkSheet.Cells[j + 1, "Z"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.flat) ? "" : apiResponse.Suggestions[x].data.address.data.flat;
+
+                        ObjWorkSheet.Cells[1, "AA"] = "Индекс";
+                        ObjWorkSheet.Cells[j + 1, "AA"] = String.IsNullOrEmpty(apiResponse.Suggestions[x].data.address.data.postal_code) ? "" : apiResponse.Suggestions[x].data.address.data.postal_code;
+
+                    }          
+                    x++;                
+                }
+               j++;
+            }                  
+           
+        ex.Visible = true; //Отобразить Excel
+
+        ex.DisplayAlerts = false; //Отключить отображение окон с сообщениями
+        ex.Application.ActiveWorkbook.SaveAs("*****.xls", Type.Missing,
+            Type.Missing, Type.Missing, Type.Missing, false, Excel.XlSaveAsAccessMode.xlNoChange,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+        ex.Quit();         
+        }
+    }
+}
+
